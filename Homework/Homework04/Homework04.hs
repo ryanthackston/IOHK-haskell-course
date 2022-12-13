f1d573dcb0d991cac1e92ec11357767e2b44905b
ryanthackston <34466711+ryanthackston@users.noreply.github.com>
2022-12-12 23:13:36 -0600

    Update Homework04.hs

--- Homework/Homework04/Homework04.hs
+++ Homework/Homework04/Homework04.hs
@@ -62,3 +62,7 @@ tailEmp myList = case myList of
 -- write a case expression wrapped in a function that takes an Int and adds one if it's even. Otherwise does nothing. 
 -- (Use the `even` function to check if the number is even.)
 
+evenOneMore :: Int -> Int
+evenOneMore x = case even x of
+    True -> x + 1
+    False -> x
