7a6b42edd4bdd9b06ec8142e5a1246bc03b84bdc
ryanthackston <34466711+ryanthackston@users.noreply.github.com>
2022-12-12 18:47:04 -0600

    Update Homework04.hs

--- Homework/Homework04/Homework04.hs
+++ Homework/Homework04/Homework04.hs
@@ -1,15 +1,37 @@
 -- Question 1
--- Lets say you have the nested values defined bellow. How would you get the value of
+-- Lets say you have the nested values defined below. How would you get the value of
 -- 4 by using only pattern matching in a function?
 
 nested :: [([Int], [Int])]
 nested = [([1,2],[3,4]), ([5,6],[7,8])]
+
+checkFor4 :: [([Int], [Int])] -> String
+checkFor4 [([_,_],[_,x]),([_,_],[_,_])] = show x
+checkFor4 _ = "Wrong input list!"
+
+--checkFor4 [([x==4:_],[_:_]):([_:_],[_:_])] || [([_:y==4],[_:_]):([_:_],[_:_])] = "The first list of the first tuple has 4"
+
+--checkFor4 ((fst $ nested !! 0) !! 0 ==4) || ((fst $ nested !! 0) !! 1 ==4) = "The first list of the first tuple has 4"
+--checkFor4 ((snd $ nested !! 0) !! 0 ==4) || ((snd $ nested !! 0) !! 1 ==4)  = "The second list of the first tuple has 4" where (x==4 || y==4)
+--checkFor4 ((fst $ nested !! 1) !! 0 ==4) || ((fst $ nested !! 1) !! 1 ==4)  = "The first list of the second tuple has 4" where (x==4 || y==4)
+--checkFor4 ((snd $ nested !! 1) !! 0 ==4) || ((snd $ nested !! 1) !! 1 ==4)  = "The second list of the second tuple has 4" where (x==4 || y==4)
+--checkFor4 _ = "There is no 4!"
+
+
 
 -- Question 2
 -- Write a function that takes a list of elements of any type and, if the list has 3 or more elements, it
 -- removes them. Else, it does nothing. Do it two times, one with multiple function definitions and one with
 -- case expressions.
 
+rem3Elem :: [a] -> [a]
+rem3Elem (_:_:_:xs) = xs
+rem3Elem x          = x
+
+rem3Elem' :: [a] -> [a]
+rem3Elem' list = case list of
+    (_:_:_:xs) -> xs
+    x          -> x
 
 -- Question 3
 -- Create a function that takes a 3-element tuple (all of type Integer) and adds them together
