03f3f37c0b47344da2254c8a261d641670d1ef58
ryanthackston <34466711+ryanthackston@users.noreply.github.com>
2023-01-03 17:05:31 -0600

    Update Homework07.hs

--- Homework/Homework07/Homework07.hs
+++ Homework/Homework07/Homework07.hs
@@ -1,6 +1,20 @@
 -- Question 1
 -- Investigate the `Bounded` type class. What behaviours it provides?
 
+-- Question 1
+-- Investigate the `Bounded` type class. What behaviours it provides?
+
+-- The Bounded type class has many instances including Word, Ordering, Int, Char, and Bool. 
+-- It sets a constrained limit in a function to not go past or before a certain instance.
+-- The minBound function is the beginning of the bounded region that doesn't take data beforehand
+-- The maxBound is the end of the bounded region that doesn't take data afterwards.
+
+-- type Bounded :: * -> Constraint -- Bounded constrains any data types that is listed as an instance of Bounded.
+-- Bounded types are polymorphic constants.
+
+bound3To9 :: Bounded a => a -> a
+bound3To9 = (minBound 3) && (maxBound 9)
+
 
 -- Question 2
 -- The types Int and Word bellong to the same type classes. What is the difference
