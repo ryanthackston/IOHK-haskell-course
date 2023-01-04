8e5d529d0c32abf775fe1a6ec71540eb50fcb9d1
ryanthackston <34466711+ryanthackston@users.noreply.github.com>
2023-01-03 20:58:08 -0600

    Update Homework07.hs

--- Homework/Homework07/Homework07.hs
+++ Homework/Homework07/Homework07.hs
@@ -1,24 +1,33 @@
 -- Question 1
 -- Investigate the `Bounded` type class. What behaviours it provides?
 
--- Question 1
--- Investigate the `Bounded` type class. What behaviours it provides?
-
--- The Bounded type class has many instances including Word, Ordering, Int, Char, and Bool. 
--- It sets a constrained limit in a function to not go past or before a certain instance.
--- The minBound function is the beginning of the bounded region that doesn't take data beforehand
--- The maxBound is the end of the bounded region that doesn't take data afterwards.
+-- The Bounded class is used to name the upper and lower limits of a type.
+-- The minBound function is the minimum size of the data type.
+-- The maxBound is the maximum size of the data type.
 
 -- type Bounded :: * -> Constraint -- Bounded constrains any data types that is listed as an instance of Bounded.
 -- Bounded types are polymorphic constants.
 
-bound3To9 :: Bounded a => a -> a
-bound3To9 = (minBound 3) && (maxBound 9)
+minBound :: (Bool, Int, Char)
+
+bound3To9 :: Bounded Int => Int -> Int
+    minBound = 3
+    maxBound = 9
 
 
 -- Question 2
--- The types Int and Word bellong to the same type classes. What is the difference
+-- The types Int and Word belong to the same type classes. What is the difference
 -- between them? Check maybe the maxBound and minBound parameter for both types.
+minBound :: (Word)
+maxBound :: (Word)
+
+minBound :: (Int)
+maxBound :: (Int)
+
+-- minBound and maxBound for Word and Int each gives the same range of numbers.
+-- The difference is that minBound for Word starts at 0 and maxBound is at 18446744073709551615
+-- While minBound for Int starts at -9223372036854775808 and maxBound goes to 9223372036854775808
+-- So there are definitely negative Int values but no negative Word values.
 
 
 -- Question 3
